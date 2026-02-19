bcffbcbe9855844c30c9d77dd7ad7d0ef38f5ec6
guicarva <guicarva@student.42porto.com>
2026-02-19 20:37:48 +0000

    djfksljflsa

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -6,14 +6,14 @@
 /*   By: guicarva <guicarva@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 19:11:51 by guicarva          #+#    #+#             */
-/*   Updated: 2026/02/16 19:11:09 by guicarva         ###   ########.fr       */
+/*   Updated: 2026/02/19 18:12:50 by guicarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
-	int	i1;
+	int	j;
 	int	temp;
 
 	i = 0;
@@ -22,7 +22,7 @@ void	ft_sort_int_tab(int *tab, int size)
 		j = i + 1;
 		while (j < size)
 		{
-			if (tab[i] > tab[i1])
+			if (tab[i] > tab[j])
 			{
 				temp = tab[i];
 				tab[i] = tab[j];
@@ -33,21 +33,21 @@ void	ft_sort_int_tab(int *tab, int size)
 		i++;
 	}
 }
-// 
-// #include <stdio.h>
-// 
-// int	main(void)
-// {
-// 	int	arr[] = {'0', 060, 0x30};
-// 	int	i;
-// 	int	size;
-// 
-// 	size = sizeof(arr) / sizeof(int);
-// 	ft_sort_int_tab(arr, size);
-// 	i = 0;
-// 	while (i < size)
-// 	{
-// 		printf("%d, ", arr[i]);
-// 		i++;
-// 	}
-// }
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	arr[] = {'0', 060, 0x30};
+	int	i;
+	int	size;
+
+	size = sizeof(arr) / sizeof(int);
+	ft_sort_int_tab(arr, size);
+	i = 0;
+	while (i < size)
+	{
+		printf("%d, \n", arr[i]);
+		i++;
+	}
+}
--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: guicarva <guicarva@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 08:45:28 by guicarva          #+#    #+#             */
-/*   Updated: 2026/02/11 19:15:37 by guicarva         ###   ########.fr       */
+/*   Updated: 2026/02/19 17:54:34 by guicarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,26 @@
 
 void	ft_putnbr(int nb)
 {
-	char	num;
+	char	c;
+	long	lg;
 
-	if (nb == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
-	if (nb < 0)
+	lg = nb;
+	if (lg < 0)
 	{
 		write(1, "-", 1);
-		nb = -nb;
+		lg = -lg;
 	}
-	if (nb >= 10)
-		ft_putnbr(nb / 10);
-	num = (nb % 10) + '0';
-	write(1, &num, 1);
+	if (lg >= 10)
+		ft_putnbr(lg / 10);
+	c = (lg % 10) + '0';
+	write(1, &c, 1);
 }
-// 
-// #include <stdio.h>
-// 
-// int	main(void)
-// {
-// 	ft_putnbr(-1230);
-// 	return (0);
-// }
+
+#include <stdio.h>
+
+int	main(void)
+{
+	ft_putnbr(-2147483648);
+	write(1, "\n", 1);
+	return (0);
+}
